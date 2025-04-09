63125ba264a9a6de9b008e44f10e94386e95c0a0
alex <anebotov@gmail.com>
2025-04-09 20:32:14 +0300

    Refactoring MCP 2025 Schema using Type Aliases

--- shared/mcp/2025/schema/resourceTemplate.go
+++ shared/mcp/2025/schema/resourceTemplate.go
@@ -1,17 +1,9 @@
 package schema
 
+import schema2024 "github.com/gate4ai/mcp/shared/mcp/2024/schema"
+
 // ResourceTemplate describes a template description for resources available on the server.
-type ResourceTemplate struct {
-	Annotations *Annotations `json:"annotations,omitempty"` // Optional annotations for the client
-	// A URI template (according to RFC 6570) that can be used to construct resource URIs.
-	URITemplate string `json:"uriTemplate"` // @format uri-template
-	// A human-readable name for the type of resource this template refers to.
-	Name string `json:"name"`
-	// A description of what this template is for.
-	Description string `json:"description,omitempty"`
-	// The MIME type for all resources that match this template.
-	MimeType string `json:"mimeType,omitempty"`
-}
+type ResourceTemplate = schema2024.ResourceTemplate
 
 // ListResourceTemplatesRequest requests a list of resource templates.
 // Sent from the client to request a list of resource templates the server has.
