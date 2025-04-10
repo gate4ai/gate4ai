472e7e99081ae08b0ce9ff429cf05db0fbdff763
alex <anebotov@gmail.com>
2025-04-10 21:19:00 +0300

    add log

--- portal/server/middleware/log-config.ts
+++ portal/server/middleware/log-config.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler((event) => {
-    const config = useRuntimeConfig(event) // Передаем event для серверных middleware
+    const config = useRuntimeConfig(event) 
     console.log('[Middleware log-config] JWT Secret on request:', config.jwtSecret);
     console.log('[Middleware log-config] Type of jwtSecret:', typeof config.jwtSecret);
     console.log('[Middleware log-config] process.env.JWT_SECRET:', process.env.JWT_SECRET);
